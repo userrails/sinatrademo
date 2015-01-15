550227d280e39fe6861146449fff1db0bcfaf061
rails <rails@rails>
2015-01-15 15:29:45 +0530

    initial commit

---
+++ app/controllers/list.rb
@@ -0,0 +1,32 @@
+Cast::App.controllers :list do
+  
+  # get :index, :map => '/foo/bar' do
+  #   session[:foo] = 'bar'
+  #   render 'index'
+  # end
+
+  # get :sample, :map => '/sample/url', :provides => [:any, :js] do
+  #   case content_type
+  #     when :js then ...
+  #     else ...
+  # end
+
+  # get :foo, :with => :id do
+  #   'Maps to url '/foo/#{params[:id]}''
+  # end
+
+  # get '/example' do
+  #   'Hello world!'
+  # end
+  
+  get :new do
+    render 'list/new'
+  end
+
+  get :index do
+    List.create(params)
+    @orders = List.all
+    render 'list/index'
+  end
+
+end
---
+++ app/helpers/list_helper.rb
@@ -0,0 +1,13 @@
+# Helper methods defined here can be accessed in any controller or view in the application
+
+module Cast
+  class App
+    module ListHelper
+      # def simple_helper_method
+      # ...
+      # end
+    end
+
+    helpers ListHelper
+  end
+end
